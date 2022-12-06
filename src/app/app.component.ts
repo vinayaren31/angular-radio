5199f25097ffad33e9047975b15e4fb5ec7bd898
vinayaren31 <119921053+vinayaren31@users.noreply.github.com>
2022-12-06 13:13:20 +0530

    1 binaries uploaded

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component, VERSION } from '@angular/core';
+import { Component } from '@angular/core';
+import PSPDFKit from 'pspdfkit';
 
 @Component({
   selector: 'my-app',
@@ -6,6 +7,19 @@ import { Component, VERSION } from '@angular/core';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  name = 'Angular ' + VERSION.major;
-  user = 'vinay';
+  title = 'PSPDFKit for Web Angular Example';
+
+  ngAfterViewInit() {
+    PSPDFKit.load({
+      // Use the assets directory URL as a base URL. PSPDFKit will download its library assets from here.
+      baseUrl: location.protocol + '//' + location.host + '/assets/',
+      document: '/assets/Document.pdf',
+      container: '#pspdfkit-container',
+    }).then((instance) => {
+      // For the sake of this demo, store the PSPDFKit for Web instance
+      // on the global object so that you can open the dev tools and
+      // play with the PSPDFKit API.
+      (window as any).instance = instance;
+    });
+  }
 }
